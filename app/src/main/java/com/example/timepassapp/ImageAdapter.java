6861a37c9aa90a916c5ed3eaa2f7633fd40af769
vishal <rajgorvishal28@gmail.com>
2023-03-05 23:38:14 +0530

    final commit

---
+++ app/src/main/java/com/example/timepassapp/ImageAdapter.java
@@ -0,0 +1,4 @@
+package com.example.timepassapp;
+
+public class ImageAdapter {
+}
---
+++ app/src/main/java/com/example/timepassapp/Imagenew.java
@@ -0,0 +1,30 @@
+package com.example.timepassapp;
+
+public class Imagenew {
+    private String name;
+    private String url;
+
+    public Imagenew() {
+    }
+
+    public Imagenew(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
